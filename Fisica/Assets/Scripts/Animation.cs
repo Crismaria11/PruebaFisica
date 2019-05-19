909f12af3f921fe25f016e2238db15f32cafa113
Cristina B <bau161260@uvg.edu.gt>
2019-05-19 11:44:31 -0600

    Intento de proyecto

---
+++ Fisica/Assets/Scripts/Animation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Animation : MonoBehaviour
+{
+    public Texture2D[] gif;
+    public int frames = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int indice = (int)(Time.time * frames) % gif.Length;
+        GetComponent<RawImage>().texture = gif[indice];
+    }
+}
